0ab46f274b3d41f0f342f87f9860945c72e249ca
Daniel Vysotskyi <vysotskyi.daniel@gmail.com>
2019-08-12 15:49:41 +0300

    fix app crach

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -61,7 +61,7 @@ module Api
           ENV.fetch("TWILIO_API_KEY"),
           ENV.fetch("TWILIO_API_SECRET"),
           [grant],
-          identity: current_user.email
+          identity: current_user.email,
           ttl: 5.minutes.to_i
         )
         render json: { token: token.to_jwt }
