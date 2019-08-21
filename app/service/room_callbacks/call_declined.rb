14737430e143862762f0457c239138b83a8d5fe8
Daniel Vysotskyi <vysotskyi.daniel@gmail.com>
2019-08-21 15:28:25 +0300

    Add new step to Decliner service

--- app/service/room_callbacks/call_declined.rb
+++ app/service/room_callbacks/call_declined.rb
@@ -3,12 +3,23 @@ class RoomCallbacks::CallDeclined
   def self.call(group_name:, declined_by_email:)
     with(
       declined_by_email: declined_by_email,
-      callee_email:      group_name.split("\\").first,
-      caller_email:      group_name.split("\\").last
+      group_name:        group_name
     ).reduce(
+      RoomCallbacks::CallDeclined::SetCalleeAndCallerEmail,
       RoomCallbacks::CallDeclined::FindCallParticipants,
       RoomCallbacks::CallDeclined::SendPush
     )
+  end
+
+  class RoomCallbacks::CallDeclined::SetCalleeAndCallerEmail
+    extend LightService::Action
+    expects :group_name
+    promises :callee_email, :caller_email
+
+    executed do |context|
+      context.callee_email = context.group_name.split("\\").first
+      context.caller_email = context.group_name.split("\\").last
+    end
   end
 
   class RoomCallbacks::CallDeclined::FindCallParticipants
